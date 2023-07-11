86011c41a302e526554320139121ed6e35fec6b2
rrarr <rrarrk123@gmail.com>
2023-07-11 18:59:51 +0300

    database edit, add Post and import post for database

--- WebApp/blog/views.py
+++ WebApp/blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
-posts = [
-    {
-        'author': 'CorresyMS',
-        'title': 'Blog Post1',
-        'content': 'First post content',
-        'data_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post1',
-        'content': 'Second post content',
-        'data_posted': 'August 28, 2018'
-    },
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
